d6c46c2200daf257b6fc70e1ee23ada28cd29c6e
Horacio Barrios <horaciobarrios1996@gmail.com>
2025-01-08 01:04:58 -0600

    Cambiando estructura del código

--- ecuaciones_de_estado.py
+++ ecuaciones_de_estado.py
@@ -5,28 +5,62 @@ para ecuaciones cúbicas de estado de sustancias puras
 3. Ecuación de Soave-Redlich-Kwong
 4. Ecuación de Peng-Robinson"""
 
+"""Referencia: 
+Introduction to Chemical Engineering Thermodynamics, 
+by J.M. Smith, H.C. Van Ness, M.M. Abott"""
+
+texto = """
+        Calculadora de ecuaciones de estado cúbicas\n
+        Ecuaciones disponibles:\n
+        1. Van der Waals\n
+        2. Redlich-Kwong\n
+        3. Soave-Redlich-Kwong\n
+        4. Peng-Robinson\n
+        """
+print(texto)
+
+R = 8.314 #J/mol-K
+print("Constante universal de gases utilizada para cálculos: " + R + " J/mol-K \n" )
+
+print("Ingrese propiedades termodinámicas de la sustancia\n")
+while comprobante == 1:
+    Tc = input("Ingrese Tc de la sustancia en K:\n")
+    Pc = input("Ingrese Pc de la sustancia en Pa:\n")
+    omega = input("Ingrese factor acéntrico de la sustancias:\n")
+    if Tc < 0:
+        print("Tc inválida, no existen Temperaturas absolutas negativas\n")
+        comprobante = 1
+    elif Pc < 0:
+        print("Tc inválida, no existen presiones negativas\n")
+        comprobante = 1
+    else:
+        comprobante = 0
+        
+        
+
 def ecuacion_de_estado():
     contador = 1
     while contador == 1:
-        texto = """
-        Calculadora de ecuaciones de estado cúbicas
-        Seleccione una opción para calcular con la EdE:
-        1. Van der Waals
-        2. Redlich-Kwong
-        3. Soave-Redlich-Kwong
-        4. Peng-Robinson
-        """
-        print(texto)
-        opcion = input("Ingrese opción válida: ")
+        
+        opcion = input("Ingrese opción válida: \n")
         
         match opcion:
             case 1:
+                print("Ecuación de van der Waals seleccionada\n")
+                
                 
             case 2:
+                print("Ecuación de Redlich-Kwong seleccionada\n")
+                
                 
             case 3:
+                print("Ecuación de Soave-Redlich-Kwong seleccionada\n")
                 
             case 4:
+                print("Ecuación de Peng-Robinson seleccionada\n")
                 
             case _:
-                print("")
+                print("Entrada inválida, intente de nuevo\n")
+                
+                
+ecuacion_de_estado()
